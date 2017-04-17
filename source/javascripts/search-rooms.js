598b35d948cf9d81c704638f8186dcd2cbe3db37
Kelton Manzanares <keltonmanzanares@gmail.com>
2017-04-17 12:24:10 -0600

    including nights in booking widget

--- source/javascripts/search-rooms.js
+++ source/javascripts/search-rooms.js
@@ -3,13 +3,19 @@
 
   $(function() {
     $('.js-search-rooms').on('click',function() {
-      var checkIn = $('.js-check-in').val();
-      var checkOut = $('.js-check-out').val();
+      var checkIn = $('input.js-check-in').val();
+      var nights = $('select.js-nights').val();
+      var roomType = $('input.js-room-type');
       var bookingsUrl = 'https://bookings.frontdeskanywhere.net/#account/1ZN160825A?source=website_check_now';
 
-      if (checkIn && checkOut) {
+      if (checkIn && nights) {
+        var checkOut = moment(checkIn, 'MM/DD/YYYY').add(nights, 'days').format('MM/DD/YYYY');
         var queryString = '&checkin=' + checkIn + '&checkout=' + checkOut;
         bookingsUrl += queryString;
+      }
+
+      if (roomType.length) {
+        bookingsUrl += '&rt=' + roomType.val();
       }
 
       window.open(bookingsUrl, '_blank');
