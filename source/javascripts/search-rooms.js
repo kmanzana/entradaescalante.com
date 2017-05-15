e94b7e275617124b975a2e3b49922636af6143bb
Kelton Manzanares <keltonmanzanares@gmail.com>
2017-05-15 16:47:38 -0600

    adding OnRes integrated items

--- source/javascripts/search-rooms.js
+++ source/javascripts/search-rooms.js
@@ -1,26 +1,26 @@
 ;(function() {
   'use strict';
 
-  $(function() {
-    $('.js-search-rooms').on('click',function() {
-      var checkIn = $('input.js-check-in').val();
-      var nights = $('select.js-nights').val();
-      var roomType = $('input.js-room-type');
-      var bookingsUrl = 'https://bookings.frontdeskanywhere.net/#account/1ZN160825A?source=website_check_now';
+  // $(function() {
+  //   $('.js-search-rooms').on('click',function() {
+  //     var checkIn = $('input.js-check-in').val();
+  //     var nights = $('select.js-nights').val();
+  //     var roomType = $('input.js-room-type');
+  //     var bookingsUrl = 'https://bookings.frontdeskanywhere.net/#account/1ZN160825A?source=website_check_now';
 
-      if (checkIn && nights) {
-        var checkOut = moment(checkIn, 'MM/DD/YYYY').add(nights, 'days').format('MM/DD/YYYY');
-        var queryString = '&checkin=' + checkIn + '&checkout=' + checkOut;
-        bookingsUrl += queryString;
-      }
+  //     if (checkIn && nights) {
+  //       var checkOut = moment(checkIn, 'MM/DD/YYYY').add(nights, 'days').format('MM/DD/YYYY');
+  //       var queryString = '&checkin=' + checkIn + '&checkout=' + checkOut;
+  //       bookingsUrl += queryString;
+  //     }
 
-      if (roomType.length) {
-        bookingsUrl += '&rt=' + roomType.val();
-      }
+  //     if (roomType.length) {
+  //       bookingsUrl += '&rt=' + roomType.val();
+  //     }
 
-      window.open(bookingsUrl, '_blank');
+  //     window.open(bookingsUrl, '_blank');
 
-      return false;
-    });
-  });
+  //     return false;
+  //   });
+  // });
 })();
